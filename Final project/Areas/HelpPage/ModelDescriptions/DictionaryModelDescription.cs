eb0b6a45ad76b0be923faebab0b3dd35a530d4b1
ShaniSherman <46422573+ShaniSherman@users.noreply.github.com>
2019-01-06 18:16:00 +0200

    Add project files.

---
+++ Final project/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Final_project.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Final project/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Final_project.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
